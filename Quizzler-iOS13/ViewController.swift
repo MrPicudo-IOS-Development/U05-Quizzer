e5a6ffccc51893c36f8fb5fccf0ba4cc908a8096
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-01-07 00:06:25 -0600

    Conexiones básicas
    Se agregaron IBOutlets para cada elemento de la interfaz y una IBAction para los dos botones, a los cuales se les dio la funcionalidad de cambiar el atributo de "text" de la UILabel que se conectó. El texto que se cambia se obtiene de un array que se creó directamente en el código sin conectarlo a la interfaz.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,20 +1,46 @@
-//
-//  ViewController.swift
-//  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     
+    // Variables conectadas a la interfaz
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    // Arreglo de preguntas para nuestra aplicación
+    let quiz = ["4 + 2 = 6", "5 - 3 = 1", "3 + 8 = 10",
+                "7 x 12 = 84", "9 x 21 = 188", "3 x 17 = 51",
+                "91 ÷ 7 = 13", "198 ÷ 9 = 23", "121 ÷ 11 = 12"]
+
+    // Variable que controla la pregunta en la que nos encontramos.
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // questionLabel.text = quiz[Int.random(in: 0...5)]
+        updateUI()
     }
 
+    // Función para los botones de true y false
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        // print(sender.currentTitle!)
+        // Si el número de pregunta todavía no llega al número de preguntas totales menos 1...
+        if(questionNumber < quiz.count-1) {
+            questionNumber += 1
+        }
+        else {
+            questionNumber = 0
+        }
+        updateUI()
+    }
+    
+    // Función para cambiar la pregunta mostrada en la interfaz
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
+    
+    
 
 }
 
