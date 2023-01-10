22b87b032b983b2adf47b3a3578a6bde718a0d9c
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-01-10 06:33:57 -0600

    Retroalimentación de la pregunta seleccionada
    Se creó un arreglo bidimensional para relacionar cada pregunta con una respuesta y se agregó el código para mostrar temporalmente la retroalimentación de la respuesta seleccionada por el usuario.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -8,10 +8,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    // Arreglo de preguntas para nuestra aplicación
-    let quiz = ["4 + 2 = 6", "5 - 3 = 1", "3 + 8 = 10",
-                "7 x 12 = 84", "9 x 21 = 188", "3 x 17 = 51",
-                "91 ÷ 7 = 13", "198 ÷ 9 = 23", "121 ÷ 11 = 12"]
+    // Arreglo bidimensional de preguntas para nuestra aplicación
+    let quiz = [ ["4 + 2 = 6", "True"], ["5 - 3 = 1", "False"], ["3 + 8 = 10", "False"],
+                 ["7 x 12 = 84", "True"], ["9 x 21 = 188", "False"], ["3 x 17 = 51", "True"],
+                ["91 ÷ 7 = 13", "True"], ["198 ÷ 9 = 23", "False"], ["121 ÷ 11 = 12", "False"] ]
 
     // Variable que controla la pregunta en la que nos encontramos.
     var questionNumber = 0
@@ -25,6 +25,15 @@ class ViewController: UIViewController {
     // Función para los botones de true y false
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         // print(sender.currentTitle!)
+        let userAnswer = sender.currentTitle! // Puede ser "True" o "False"
+        let correctAnswer = quiz[questionNumber][1]
+        if (userAnswer == correctAnswer) {
+            questionLabel.text = "Correct!" // Actualizamos el texto de la etiqueta con el resultado de la respuesta elegida.
+        }
+        else {
+            questionLabel.text = "Try again!"
+            questionNumber -= 1
+        }
         // Si el número de pregunta todavía no llega al número de preguntas totales menos 1...
         if(questionNumber < quiz.count-1) {
             questionNumber += 1
@@ -32,15 +41,21 @@ class ViewController: UIViewController {
         else {
             questionNumber = 0
         }
-        updateUI()
+        // El siguiente método hace que el programa espere 1.5 segundos para realizar lo que tenga en su bloque de código.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.updateUI() // Actualizamos la etiqueta a la siguiente pregunta.
+        }
     }
     
     // Función para cambiar la pregunta mostrada en la interfaz
     func updateUI() {
-        questionLabel.text = quiz[questionNumber]
+        questionLabel.text = quiz[questionNumber][0]
     }
     
-    
-
 }
 
+/* Código creado para practicar:
+ 
+ // Primero habíamos creado las preguntas y sus respuestas con un arreglo bidimensional:
+ 
+ */
