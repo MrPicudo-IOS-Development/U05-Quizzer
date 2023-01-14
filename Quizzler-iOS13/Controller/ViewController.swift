f4e1ee8535f861a167f653d5054f85c2eec77e96
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-01-13 20:58:20 -0600

    1ra implementación del MVC
    Se crearon las carpetas del MVC y se comenzaron a ordenar los archivos para separar la lógica y los datos del controlador.

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -8,23 +8,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    // Arreglo de objetos tipo Question para la aplicación, creados por Angela.
-    let quiz = [Question(q: "A slug's blood is green.", a: "True"),
-                Question(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-                Question(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-                Question(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-                Question(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-                Question(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-                Question(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-                Question(q: "Google was originally called 'Backrub'.", a: "True"),
-                Question(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-                Question(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-                Question(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-                Question(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-]
-
-    // Variable que controla la pregunta en la que nos encontramos.
-    var questionNumber = 0
+    var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,10 +18,11 @@ class ViewController: UIViewController {
 
     // Función para los botones de true y false
     @IBAction func answerButtonPressed(_ sender: UIButton) {
-        // print(sender.currentTitle!)
-        let userAnswer = sender.currentTitle! // Puede ser "True" o "False"
-        // Guardamos la respuesta correcta del arreglo de objetos.
-        let correctAnswer = quiz[questionNumber].answer
+        let userAnswer = sender.currentTitle! // Puede ser "True" o "False", nunca va a ser nulo.
+        // Mandamos a llamar a la función para revisar la respuesta del usuario.
+        quizBrain.checkAnswer(userAnswer)
+        
+        
         // Hacemos ligeramente transparente el botón presionado y modificamos la fuente del botón presionado.
         sender.alpha = 0.5
         sender.titleLabel?.font = UIFont.boldSystemFont(ofSize: 27.0)
